64df0931f2999146cae5d659a09562755e852ac8
fjdelpino <fjdelpino@atsistemas.com>
2022-05-10 11:09:26 +0200

    Primeros pasos, fundamentos

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,7 @@
+// alert('Hola Mundo');
+
+// let nombre = prompt('¿Cuál es tu nombre?');
+// console.log('****' + nombre + '****');
+
+const seleccion = confirm('¿Estás seguro de borrar esto?');
+console.log(seleccion);
