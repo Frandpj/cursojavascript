382367ccb000e30c91f5abd84a753c6abf7cef17
fjdelpino <fjdelpino@atsistemas.com>
2022-05-11 17:10:25 +0200

    Blackjack terminado

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -14,6 +14,11 @@ let puntosPc = 0;
 
 // Referencias del HTML
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+const btnNuevo = document.querySelector('#btnNuevo');
+
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasPc = document.querySelector('#pc-cartas');
 const marcadores = document.querySelectorAll('small');
 
 // Crear una nueva baraja
@@ -54,12 +59,86 @@ const valorCarta = (carta) => {
     : valor * 1;
 }
 
+// Turno del pc
+const turnoPc = (puntosMinimos) => {
+    do {
+        const carta = pedirCarta();
+
+        // Suma los puntos del pc
+        puntosPc = puntosPc + valorCarta(carta);
+        marcadores[1].innerText = puntosPc;
+        
+        // Crea la imagen de la carta
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${carta}.png`;
+        imgCarta.classList.add('carta');
+
+        // Inserta la imagen de la carta
+        divCartasPc.append(imgCarta);
+
+        if (puntosMinimos > 21) {
+            break;
+        }
+    } while ((puntosPc < puntosMinimos) && (puntosMinimos <= 21));
+
+    setTimeout(() => {
+        puntosPc === puntosMinimos ? alert('Nadie gana') :
+        puntosMinimos > 21  ? alert('El PC gana') :
+        puntosPc > 21 ? alert('Jugador 1 gana') : alert('El PC gana');
+    }, 100);
+
+}
+
+const nuevoJuego = () => {
+    deck = [];
+    crearDeck();
+    puntosJugador = 0;
+    puntosPc = 0;
+    divCartasJugador.innerHTML = '';
+    divCartasPc.innerHTML = '';
+    marcadores[0].innerText = 0;
+    marcadores[1].innerText = 0;
+}
+
 // Eventos
 btnPedir.addEventListener('click', () => {
+    // Recoge la carta pedida
     const carta = pedirCarta();
 
+    // Suma los puntos del jugador
     puntosJugador = puntosJugador + valorCarta(carta);
     marcadores[0].innerText = puntosJugador;
     
-    
+    // Crea la imagen de la carta
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+
+    // Inserta la imagen de la carta
+    divCartasJugador.append(imgCarta);
+
+    // Comprueba si el jugador se pasa de 21
+    if (puntosJugador > 21) {
+        console.warn('Lo siento mucho, perdiste');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoPc(puntosJugador);
+    } else if (puntosJugador === 21) {
+        console.warn('21, genial');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoPc(puntosJugador);
+    }
+});
+
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoPc(puntosJugador);
+});
+
+btnNuevo.addEventListener('click', () => {
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+    nuevoJuego();
 });
