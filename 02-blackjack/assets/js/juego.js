1db82fe4cfe2b3908b497410d6366d9404ad1c15
fjdelpino <fjdelpino@atsistemas.com>
2022-05-11 13:06:53 +0200

    Función de valor de la carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -41,3 +41,13 @@ const pedirCarta = () => {
 }
 
 // pedirCarta();
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return (isNaN(valor))  ? 
+    (valor === 'A') ? 11 : 10
+    : valor * 1;
+}
+
+const valor = valorCarta('AD');
+console.log({valor});
