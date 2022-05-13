8b6bd25a0c65f1feb3f5c217bfd33c8006859c9c
fjdelpino <fjdelpino@atsistemas.com>
2022-05-13 14:21:05 +0200

    Manejo de imagenes y copiar plugins

--- 03-webpack-inicial/webpack.config.js
+++ 03-webpack-inicial/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     // Modo de compilación
@@ -18,28 +19,42 @@ module.exports = {
                     sources: false
                 }
             },
-            // Cargador de archivos CSS
+            // Cargador de archivos CSS, excluyendo style.css para que se cargue con el loader de MiniCssExtractPlugin
             {
                 test: /\.css$/,
                 exclude: /styles.css$/,
                 use: [ 'style-loader', 'css-loader' ]
             },
+            // Se habilita el cargador de archivos CSS para el archivo styles.css
             {
                 test: /styles.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            // Cargador de imagenes
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader',
             }
         ]
     },
     optimization: {},
     plugins: [
+        // Plugin para crear un archivo index.html con el contenido de la carpeta dist
         new HtmlWebpackPlugin({
             title: 'Mi Webpack App',
             // filename: 'index.html',
             template: './src/index.html'
         }),
+        // Plugin para extraer CSS a un archivo aparte
         new MiniCssExtractPlugin({
             filename: "[name].css",
             ignoreOrder: false
-        })
+        }),
+        // Plugin para copiar archivos
+        new CopyPlugin({
+            patterns: [
+                { from: './src/assets', to: 'assets' }
+            ]
+        }),
     ],
 }
