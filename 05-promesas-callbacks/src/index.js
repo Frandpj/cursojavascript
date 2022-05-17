2b552844e4fb3b8e9a51230e2f72bd2d5c2e5aab
fjdelpino <fjdelpino@atsistemas.com>
2022-05-17 09:09:03 +0200

    Callbacks y callbacks hell

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -1,7 +1,23 @@
-// Impota otro archivo js
-import {saludar} from './js/componentes';
+// import { buscarHeroe } from './js/callbacks';
 import './styles.css';
 
-const nombre = 'Fran';
+const heroeId1 = 'capi';
+const heroeId2 = 'iron';
 
-saludar(nombre);
+/**
+ * Un callback es una función enviada como un argumento
+ * Esta función como vemos tiene dos parámetros y uno de ellos es una función con otros dos parámetros
+ * Lo que estamos haciendo aquí es recogiendo todos los heroes y comprobando que existen para poder recogerlos todos en un mensaje
+ * Esto es un callback hell que se puede reducir a una sola función con una promesa
+ */
+// buscarHeroe(heroeId1, (err, heroe1) => {
+//     if (err) {
+//         return console.error(err);
+//     }
+//     buscarHeroe(heroeId2, (err, heroe2) => {
+//         if (err) {
+//             return console.error(err);
+//         }
+//         console.log(`Enviando a ${heroe1.nombre} y a ${heroe2.nombre} a la misión`);
+//     });
+// });
